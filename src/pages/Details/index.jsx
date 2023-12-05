9afba6fc9c60012035b1126ec2cec98a3f57d8a1
Guilherme Denardi <gdenardi@outlook.com>
2023-12-05 18:35:06 -0300

    adding styles, fonts and configs of project

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from './styles';
+
+export function Details() {
+    return (
+        <Container>
+            <h1>Guilherme</h1>
+        </Container>
+    )
+}
---
+++ src/styles/theme.js
@@ -0,0 +1,19 @@
+export default {
+    COLORS: {
+        PINK: "#FF859B",
+
+        GRAY_100: "#CAC4CF",
+        GRAY_200: "#948F99",
+        GRAY_300: "#999591",
+
+        BLACK_100: "#312E38",
+        BLACK_200: "#1C1B1E",
+
+        WHITE: "#FFFFFF",
+        WHITE_100: "#F4EDE8",
+
+        BACKGROUND_100: "#262529",
+        BACKGROUND_200: "#1C1B1E",
+        BACKGROUND_300: "#0D0C0F",
+    }
+}
