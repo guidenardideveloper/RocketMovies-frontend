ad0cf56a947d90b07d5308d3dcadd96db138846c
Guilherme Denardi <gdenardi@outlook.com>
2023-12-20 11:56:14 -0300

    saving user session data to localStorage

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 import { api } from "../services/api";
 
@@ -13,6 +13,9 @@ function AuthProvider({ children }) {
 
             const response = await api.post("/sessions", { email, password });
             const { user, token } = response.data;
+
+            localStorage.setItem("@rocketmovie:user", JSON.stringify(user));
+            localStorage.setItem("@rocketmovie:token", token);
             
             api.defaults.headers.authorization = `Bearer ${token}`;
             setData({ user, token });
@@ -26,6 +29,22 @@ function AuthProvider({ children }) {
             }
         }
     }
+
+
+    useEffect(() => {
+        const token = localStorage.getItem("@rocketmovie:token");
+        const user = localStorage.getItem("@rocketmovie:user");
+
+        if (token && user) {
+            api.defaults.headers.authorization = `Bearer ${token}`;
+
+            setData({
+                token,
+                user: JSON.parse(user)
+            });
+        }
+        
+    }, []);
 
     return (
         <AuthContext.Provider value={{ signIn, user: data.user }}>
